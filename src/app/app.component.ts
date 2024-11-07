fc840d56e3b6aea53f8a4d8961f12c099633efe4
Jorge Cano <jorgeucano@gmail.com>
2024-11-07 10:25:59 -0300

    set full demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, effect, inject, resource, ResourceStatus } from '@angular/core';
+import { Component, effect, inject, resource, ResourceRef, ResourceStatus, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
 import { LoaderSpinnerComponent } from './loading-spinner/app.loading-spinner.component';
+import { Card, Cards } from './interfaces/mtg-interface';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +20,15 @@ export class AppComponent {
   adviceResource;
   adviceRxResource;
   resourceStatus = ResourceStatus;
+
+  mtgCards: Card[] = [];
+  mtgCard: Card | undefined;
+
+
+  mtgCardsSignals: ResourceRef<any>;
+  mtgCardSignals: any;
+
+  mtgSelectedId = signal<number | undefined> (undefined);
 
   constructor() {
     this.http
@@ -50,9 +60,56 @@ export class AppComponent {
     effect(() => {
       console.log('RxResource code:' , ResourceStatus[this.adviceRxResource.status()]);
       console.log('RxResource value:', this.adviceRxResource.value()?.slip.advice); 
-    })
+    });
 
+    // multiples
+
+    this.http.get<Cards>('https://api.magicthegathering.io/v1/cards')
+    .subscribe(
+      (value) => {
+        console.log(value.cards);
+        const mixedArray = [...value.cards].sort(() => 0.5 - Math.random());
+        this.mtgCards = mixedArray.slice(0, 10);
+      }
+    );
+
+
+    // mtg
+
+    // RXRESOURCE
+    this.mtgCardsSignals = rxResource({
+      loader: () =>  this.http.get<any>('https://api.magicthegathering.io/v1/cards')
+    });
+
+    effect(() => {
+      console.log('RxResource code:' , ResourceStatus[this.mtgCardsSignals.status()]);
+      console.log('RxResource value:', this.mtgCardsSignals.value()?.cards); 
+    });
+
+
+    this.mtgCardSignals = rxResource({
+      request: () =>  this.mtgSelectedId(),
+      loader: ({ request: multiverseid}) => this.http.get(`https://api.magicthegathering.io/v1/cards/${multiverseid}`)
+    });
+
+    effect(() => {
+      console.log('mtgCardSignals RxResource code:' , ResourceStatus[this.mtgCardSignals.status()]);
+      console.log('mtgCardSignals RxResource value:', this.mtgCardSignals.value()); 
+    });
     
+  }
+
+  getCard(id: number) {
+    console.log(id);
+    this.mtgSelectedId.set(id);
+    // this.http.get(`https://api.magicthegathering.io/v1/cards/${id}`)
+    // .subscribe(
+    //   (value: any) => {
+    //     console.log(value);
+    //     this.mtgCard = value.card;
+    //     console.log(this.mtgCard);
+    //   }
+    // );
   }
 
 
---
+++ src/app/interfaces/mtg-interface.ts
@@ -0,0 +1,34 @@
+export interface Card {
+    name: string;
+    names?: string[];
+    manaCost: string;
+    cmc: number;
+    colors: string[];
+    colorIdentity: string[];
+    type: string;
+    supertypes?: string[];
+    types: string[];
+    subtypes?: string[];
+    rarity: string;
+    set: string;
+    text: string;
+    artist: string;
+    number: string;
+    power: string;
+    toughness: string;
+    layout: string;
+    multiverseid: number;
+    imageUrl:   
+   string;
+    rulings?: { date: string; text: string }[];
+    foreignNames?: { name: string; language: string; multiverseid: number }[];
+    printings: string[];
+    originalText: string;
+    originalType: string;
+    id:   
+   string;
+  }
+
+  export interface Cards {
+    cards: Card[];
+  }
